808a0ddde4f06cfad9acedcaa4fa5892b70b102b
Jesus Coronado <jesus.coronado@geekbears.com>
2023-10-11 13:54:35 -0700

    fix!: `MeasurementResult` class changes & custom exceptions
    `leftTire`, `middleTire` and `rightTire` now are nullable properties from `MeasurementResult` class.
    
    `AnylineTireTreadScannerException` has been integrated for handling custom exceptions that can not be considered as a `PlatformException`.

--- lib/flutter_anyline_tire_tread_scanner.dart
+++ lib/flutter_anyline_tire_tread_scanner.dart
@@ -1,4 +1,5 @@
 export 'src/core/anyline_tire_tread_scanner.dart';
+export 'src/core/exceptions.dart';
 export 'src/core/measurement_system.dart';
 export 'src/core/scanning_event.dart';
 export 'src/core/tread_depth_result.dart';
